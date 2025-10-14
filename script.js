2f7a4eb18ca3f03ddf51d81a2197629b241e9623
Andrewbizz <ezeaniandrew8197@gmail.com>
2025-10-14 12:44:51 +0100

    adding, mioving and deleting elemeents

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { cloneElement } = require('react');
+
 ///////////////////////////////////////
 // Modal window
 
@@ -8,7 +10,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +21,14 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn, i) => {
+  btn.addEventListener('click', openModal);
+});
 
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+//selecting elements in js
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +37,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+//
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+// creating elements in js
+
+const message = document.createElement('div');
+
+message.classList.add('cookie-message');
+// message.textContent = 'we use cookies for improved functionality and analytics';
+
+message.innerHTML = `we use cookies for improved functionality and analytics' <button class ='btn btn--close-cookie'> Got it </button>;`;
+
+header.prepend(message);
+// header.append(message);
+// console.log(message);
+// header.append(message.cloneNode(true));
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+
+// /removing elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click', e => {
+  e.preventDefault();
+
+  message.remove();
+});
+
+//insertadjacent html is better
